6ec43e1c7fe40554b30a4480fb169e014519e90a
keysmusician <justin.masayda@gmail.com>
2021-06-16 04:17:49 +0000

    Finish quicksort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,68 +1,64 @@
 #include "sort.h"
-
-/**
- * partition - Partition an array of integers for use in the Quick sort
- * algorithm.
- * @array: array
- * @start: start index
- * @end: end index
- * Return: next pivot index
- */
-size_t partition(int *array, size_t start, size_t end)
-{
-	size_t i, pivot_index = start;
-	int temp, pivot = array[end];
-
-	for (i = start; i < end; i++)
-	{
-		if (array[i] < pivot)
-		{
-			/* Swap array[i] <-> array[pivot_index] */
-			temp = array[i];
-			array[i] = array[pivot_index];
-			array[pivot_index] = temp;
-			print_array(array, end);
-
-			pivot_index++;
-		}
-	}
-	/* Swap array[end] <-> array[pivot_index] */
-	temp = array[end];
-	array[end] = array[pivot_index];
-	array[pivot_index] = temp;
-
-	return (pivot_index);
-}
-
-/**
- * quicksort - Sort an array of integers in ascending order using the
- * Quick sort algorithm.
- * @array: array
- * @start: start index
- * @end: end index
- */
-void quicksort(int *array, size_t start, size_t end)
-{
-	size_t pivot_index;
-
-	if (start < end && end > 0)
-	{
-		pivot_index = partition(array, start, end);
-		quicksort(array, start, pivot_index - 1);
-		quicksort(array, pivot_index + 1, end);
-	}
-}
-
 /**
  * quick_sort - Sort an array of integers in ascending order using the
  * Quick sort algorithm.
  * @array: array
  * @size: size of array
+ * // quicksort(array, 0, size);
  */
 void quick_sort(int *array, size_t size)
 {
+	/* Indicies of pivot, item from left, item from right respectively */
+	size_t P, L, R;
+	int temp;
+	static int *a, S = -1;
+
+	/* Nothing to do if array is NULL or size < 2 */
 	if (!array || size < 2)
 		return;
 
-	quicksort(array, 0, size);
+	/* Save array pointer and size for printing */
+	if (S == -1)
+	{
+		a = array;
+		S = size;
+	}
+
+	/* Set pivot index to always be at the end of the list */
+	P = size - 1;
+
+	while (1)
+	{
+		/* Set L; Find the first value from the left larger than the pivot */
+		for (L = 0; array[L] < array[P]; L++)
+			continue;
+
+		/* Set R; Find the first value from the right larger than the pivot */
+		for (R = P - 1; array[R] > array[P] && R > 0; R--)
+			continue;
+
+		if (L < R)
+		{
+			/* Swap L & R */
+			temp = array[L];
+			array[L] = array[R];
+			array[R] = temp;
+			/* Display  */
+			print_array(a, S);
+		}
+		else
+			break;
+	}
+	if (L != P)
+	{
+		/* Swap L & P if they're not the same */
+		temp = array[L];
+		array[L] = array[P];
+		array[P] = temp;
+		/* array[L] is now sorted */
+		print_array(a, S);
+	}
+	/* Quick sort the new partitions above and below L */
+	quick_sort(array + L + 1, size - (L + 1));
+	quick_sort(array, L);
 }
